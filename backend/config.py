8869df9f918f9fe2a3a04517fa1109ee9c76a1c7
varshini2-lang <145186068+varshini2-lang@users.noreply.github.com>
2024-02-20 14:12:32 +0530

    config file updated

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+
+
+from typing import List
+
+class AppConfig:
+    ORIGINS = ["*"]  # Change this to the appropriate origin(s) of your React Native app
+    STORAGE_BASE_URL = "https://storage.cialabs.tech"
+
+# Add more configuration settings as needed
