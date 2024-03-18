ed3690ed1467ce360bf06bf2055a97b769a67601
KrunalDevganiya02 <krunaldevganiya02@gamil.com>
2024-03-18 22:22:34 +0530

    modified:   backend/service/service.py

--- backend/service/service.py
+++ backend/service/service.py
@@ -10,6 +10,7 @@ from database.database import collectionMeta,collectionResult
 from ciaos import save
 from typing import List 
 from models.model import Feedback,Metadata
+import pybase64
 
 
 
@@ -30,16 +31,16 @@ def test_model_v1(base64_str: str, model_name: str):
     except (ValueError, requests.exceptions.RequestException) as e:
         raise HTTPException(status_code=500, detail=f"Error sending request to MAS service: {str(e)}")
 
+
 def test_model_v2(file: UploadFile):
     try:
         files = {'file': (file.filename, file.file.read(), file.content_type)}
         masResponse = requests.post(f"{AppConfig.MAS_SERVICE_URL}{AppConfig.MAS_SERVICE_ENDPOINT}", files=files)
         
-        binary_data =file.file.read()
-        encoded = binascii.b2a_base64(binary_data, newline=False)
-        base64_string=encoded.decode('utf-8')
-       
-        response = save(AppConfig.STORAGE_BASE_URL, "", base64_string)
+        file.file.seek(0)
+        base64_encoded = pybase64.b64encode(file.file.read()).decode("utf-8")
+
+        response = save(AppConfig.STORAGE_BASE_URL, "", base64_encoded)
         json_response = response.json()
         key = json_response.get('key') 
         print(key)
@@ -47,7 +48,7 @@ def test_model_v2(file: UploadFile):
         if masResponse.status_code == 200:
             try:
                 result = masResponse.json()
-                return {"apiResult": "No" if result == 0 else "Yes", "imageKey": key}
+                return {"apiResult": "No" if result == 0 else "Yes","imageKey": key}
             except ValueError:
                 return {"error": "Failed to parse JSON response"}
         else:
